4aa16c69c74be2db4f3ff8231ec746ed6917f130
Lia <lialimafigueira@gmail.com>
2025-08-07 12:22:05 -0300

    Add files via upload

---
+++ java01.js
@@ -0,0 +1,14 @@
+alert ('Boas vindas ao nosso site!');
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+let nome = prompt('Digite o seu nome');
+
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
